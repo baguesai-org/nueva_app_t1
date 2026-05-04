fe50dcd43e2a727c99c1194ccdb0ea6428e02a5e
baguesai <baguesai@bagues.net.test-google-a.com>
2026-05-04 11:06:32 -0300

    make app persistent http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello from nueva_app_t1');
+const http = require("http");
+
+const PORT = process.env.PORT || 3100;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  res.end(`
+    <h1>nueva_app_t1 funcionando</h1>
+    <p>Deploy desde GitHub OK.</p>
+  `);
+});
+
+server.listen(PORT, "127.0.0.1", () => {
+  console.log(`Servidor escuchando en http://127.0.0.1:${PORT}`);
+});
